ebedcfaef8f41074f0246f646df58c0d0bd271ba
Achim Lenz <hello.achim@outlook.com>
2023-11-06 11:09:56 +0100

    Add description for methods

---
+++ src/main/java/bootcamp/neuefische/Passwordvalidation.java
@@ -0,0 +1,29 @@
+package bootcamp.neuefische;
+
+public class Passwordvalidation {
+
+    // Methode passwordGreater7 stellt sicher, dass das Passwort mindestens 8 Zeichen lang ist.
+    // @param password
+    // @return boolean true, wenn das Passwort mindestens 8 Zeichen hat
+    public static boolean passwordGreater7(String password) {
+
+    }
+
+
+    // Methode containNumbers prüft, ob das Passwort Ziffern enthält.
+    // @param password
+    // @return boolean true, wenn das Passwort mindestens 1 Ziffer enthält
+    public static boolean
+
+    // Methode lowerAndUpperCaseLetters stellt sicher, dass Groß- und auch Kleinbuchstaben verwendet werden.
+    // @param password
+    // @return boolean true, wenn das Passwort mindestens 1 Groß- und auch Kleinbuchstaben enthält
+    public static boolean
+
+    // Methode badPassword prüft, ob das Passwort zu üblich und zu einfach ist.
+    // @param password
+    // @return boolean true, wenn das Passwort nicht ein übliches Passwort ist
+    public static boolean
+
+
+}
